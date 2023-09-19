b0a465100b23b54501e5d7e8bed02b4a3c4aa2dc
k0robova <alonakorobova@ukr.net>
2023-09-19 23:02:31 +0300

    hw-08 --- tasks 01 and 02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+// console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img class="gallery__image" src='${preview}' alt='${description}' />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+list.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+const gallery = new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+// event - timeupdate
+
+function onTimeUpdate(data) {
+  console.log(data);
+
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+const lastVideoData = localStorage.getItem('videoplayer-current-time');
+const parsedLastVideoData = JSON.parse(lastVideoData);
+
+player.setCurrentTime(parsedLastVideoData.seconds);
+
+function handleThrottle(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+const onThrottle = throttle(handleThrottle, 1000);
+player.on('timeupdate', onThrottle);
